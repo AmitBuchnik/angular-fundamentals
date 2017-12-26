12a02ff0d23095cb2cb6bc82ebbc4f5597b87bdc
AmitBuchnik <P0020755@ness-tech.co.il>
2017-12-26 11:19:46 +0200

    ngStyle

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,12 +6,36 @@ import { Component, Input, Output, OnChanges, EventEmitter } from '@angular/core
         <div class='well hoverwell thumbnail'>
             <h2>{{ event?.name }}</h2>
             <div>Date: {{ event?.date }}</div>
+            <div [ngStyle]="getStartTimeStyle()" [ngSwitch]='event?.time'>
+                Time: {{ event?.time }}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
+            <!--
+            <div [style.color]="event?.time === '8:00 am' ? '#003300': '#bbb'" [ngSwitch]='event?.time'>
+                Time: {{ event?.time }}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>     
+            -->   
+            <!--
             <div [ngClass]="getStartTimeClass()" [ngSwitch]='event?.time'>
                 Time: {{ event?.time }}
                 <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
                 <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
                 <span *ngSwitchDefault>(Normal Start)</span>
-            </div>            
+            </div>    
+            -->
+            <!--
+            <div [class.green]="event?.time === '8:00 am'" [ngSwitch]='event?.time'>
+                Time: {{ event?.time }}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
+            -->               
             <div>Price: {{ event?.price | currency: 'USD': true }}</div>
             <div [hidden]='!event?.location'>
                 <span>Location: {{ event?.location?.address }}</span>
@@ -29,14 +53,19 @@ import { Component, Input, Output, OnChanges, EventEmitter } from '@angular/core
     // `]
 })
 export class EventThumbnailComponent extends OnChanges {
-    @Input()
-    event: any;
+    @Input() event: any;
     // someProperty: string = 'some value';
-    // @Output()
-    // eventClick = new EventEmitter();
+    // @Output() eventClick = new EventEmitter();
 
     ngOnChanges(): void {
 
+    }
+
+    getStartTimeStyle() {
+        if (this.event && this.event.time === '8:00 am') {
+            return { 'color': '#003300', 'font-weight': 'bold' };
+        }
+        return {};
     }
 
     getStartTimeClass() {
