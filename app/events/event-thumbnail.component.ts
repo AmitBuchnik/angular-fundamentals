2336ed7a28e0ed14054af382d395fb39f83752b7
AmitBuchnik <P0020755@ness-tech.co.il>
2017-12-26 15:43:39 +0200

    services

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -61,7 +61,7 @@ export class EventThumbnailComponent extends OnChanges {
 
     }
 
-    getStartTimeStyle() {
+    getStartTimeStyle(): any {
         if (this.event && this.event.time === '8:00 am') {
             return { 'color': '#003300', 'font-weight': 'bold' };
         }
