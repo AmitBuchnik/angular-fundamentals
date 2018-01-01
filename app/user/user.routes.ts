c2cbbc75d61cce5ed1002134b92145df9c9c64a8
AmitBuchnik <P0020755@ness-tech.co.il>
2018-01-01 15:42:19 +0200

    template based form

--- app/user/user.routes.ts
+++ app/user/user.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './login.component';
 
 export const userRoutes: Routes = [
-    { path: 'profile', component: ProfileComponent} // user/profile
+    { path: 'profile', component: ProfileComponent }, // user/profile
+    { path: 'login', component: LoginComponent } // user/profile
 ];
