e5afc082e2974a99d7ef0c9a453dcbc3d379b634
AmitBuchnik <P0020755@ness-tech.co.il>
2018-01-10 15:47:30 +0200

    template based validators

--- app/events/event-create.component.ts
+++ app/events/event-create.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { EventService, IEvent } from './shared/index';
+
 @Component({
-    template: `
-        <h1>New Event</h1>
-        <hr>
-        <div class="col-md-6">
-            <h3>[Create Event Form will go here]</h3>
-            <br/>
-            <br/>
-            <button type="submit" class="btn btn-primary">Save</button>
-            <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
-        </div>    
-    `
+    templateUrl: 'app/events/event-create.component.html',
+    styles: [`
+        em {float: right; color: #e05c65; padding-left: 10px;}
+        .error input { background-color: #e3c3c5;}
+        .error ::-webkit-input-placeholder { color: #999 }
+        .error ::-moz-placeholder { color: #999 }
+        .error :-moz-placeholder { color: #999 }
+        .error ::-ms-input-placeholder { color: #999 }
+  `]
 })
 export class EventCreateComponent {
-    isDirty: boolean = false;
+    isDirty: boolean = true;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private eventService: EventService) {
 
-    }        
+    }
+
+    saveEvent(event: IEvent): void {
+        this.eventService.saveEvent(event);
+        this.isDirty = false;
+        this.router.navigate(['/events']);
+    }
 
     cancel(): void {
         this.router.navigate(['/events']);
