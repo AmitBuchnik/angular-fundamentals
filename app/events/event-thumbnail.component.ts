a5aa4619c5e3f2ecfed3ee3c26556d4ddc10f55c
AmitBuchnik <P0020755@ness-tech.co.il>
2018-01-15 16:38:17 +0200

    pipes

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -5,8 +5,8 @@ import { IEvent } from './index';
     selector: 'event-thumbnail',
     template: `
         <div class='well hoverwell thumbnail' [routerLink]="['/events', event?.id]">
-            <h2>{{ event?.name }}</h2>
-            <div>Date: {{ event?.date }}</div>
+            <h2>{{ event?.name | uppercase }}</h2>
+            <div>Date: {{ event?.date | date:'shortDate' }}</div>
             <div [ngStyle]="getStartTimeStyle()" [ngSwitch]='event?.time'>
                 Time: {{ event?.time }}
                 <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
