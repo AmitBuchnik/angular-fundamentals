ef23b97961ba7518ddb932109dfd0255c86c2eca
Victor Bouhnik <amitbuhnik@gmail.com>
2018-01-22 19:06:38 +0200

    inject

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
+import { TOASTR_TOKEN, IToastr } from '../common/toastr.service';
 
 @Component({
   templateUrl: 'app/user/profile.component.html',
@@ -12,7 +13,7 @@ import { AuthService } from './auth.service';
     .error ::-webkit-input-placeholder { color: #999 }
     .error ::-moz-placeholder { color: #999 }
     .error :-moz-placeholder { color: #999 }
-    .error ::-ms-input-placeholder { color: #999 }
+    .error ::-ms-input-placeholder { color: #999 }  
   `]
 })
 export class ProfileComponent implements OnInit {
@@ -20,7 +21,9 @@ export class ProfileComponent implements OnInit {
   private firstName: FormControl;
   private lastName: FormControl;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService,
+    private router: Router,
+    @Inject(TOASTR_TOKEN) private toastr: IToastr) {
 
   }
 
@@ -38,7 +41,7 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues): void {
     if (this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-      this.router.navigate(['/events']);
+      this.toastr.success('Profile saved');
     }
   }
 
