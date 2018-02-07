828a330c61c96f87e66262896fdd02dc0e115da4
Amit Buhnik <P0020755@ness-tech.co.il>
2018-02-07 16:44:08 +0200

    delete

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -12,6 +12,7 @@ export class SessionListComponent implements OnChanges {
     @Input() sessions: ISession[];
     @Input() filterBy: string;
     @Input() sortBy: string;
+    @Input() eventId: number;
     visibleSessions: ISession[] = [];
 
     constructor(private voterService: VoterService,
@@ -47,9 +48,9 @@ export class SessionListComponent implements OnChanges {
 
     toggleVote(session: ISession): void {
         if (this.userHasVoted(session)) {
-            this.voterService.deleteVoter(session, this.authService.currentUser.userName);
+            this.voterService.deleteVoter(this.eventId, session, this.authService.currentUser.userName);
         } else {
-            this.voterService.addVoter(session, this.authService.currentUser.userName);
+            this.voterService.addVoter(this.eventId, session, this.authService.currentUser.userName);
         }
 
         if (this.sortBy === 'votes') {
