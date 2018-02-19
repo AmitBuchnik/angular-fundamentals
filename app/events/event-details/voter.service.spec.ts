45ab82acafaa8269f0a3cc87cd386bf6f947d0dd
Amit Buhnik <P0020755@ness-tech.co.il>
2018-02-19 12:11:36 +0200

    test service and component

--- app/events/event-details/voter.service.spec.ts
+++ app/events/event-details/voter.service.spec.ts
@@ -1,6 +1,6 @@
 import { VoterService } from './voter.service';
 import { ISession } from '../shared/event.model';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 
 describe('VoterService', () => {
     let voterService: VoterService, mockHttp;
@@ -19,6 +19,26 @@ describe('VoterService', () => {
 
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe('david');
+        });
+
+        it('should call http.delete with the right URL', () => {
+            var session = { id: 6, voters: ['puptiz', 'david'] };
+            mockHttp.delete.and.returnValue(Observable.of(false));
+
+            voterService.deleteVoter(1, <ISession>session, 'puptiz');
+
+            expect(mockHttp.delete).toHaveBeenCalledWith(`api/events/1/sessions/6/voters/puptiz`);
+        });
+    });
+
+    describe('addVoter', () => {
+        it('should call http.post with the right URL', () => {
+            var session = { id: 6, voters: ['puptiz'] };
+            mockHttp.post.and.returnValue(Observable.of(false));
+
+            voterService.addVoter(1, <ISession>session, 'david');
+
+            expect(mockHttp.post).toHaveBeenCalledWith(`api/events/1/sessions/6/voters/david`, "{}", jasmine.any(Object));
         });
     });
 });
