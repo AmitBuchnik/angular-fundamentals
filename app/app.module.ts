874a7a53477275b034456b2aa5a357c39e19f944
Amit Buhnik <P0020755@ness-tech.co.il>
2018-02-25 15:28:13 +0200

    PreloadAllModules

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { EventsAppComponent } from './events-app.component';
 
@@ -48,7 +48,7 @@ declare let jQuery: object;
         FormsModule,
         ReactiveFormsModule,
         HttpModule,
-        RouterModule.forRoot(appRoutes),
+        RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }) // optimistic bundle download (when ready)
     ],
     declarations: [
         EventsAppComponent,
