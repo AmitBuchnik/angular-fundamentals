cbdc8a530ca45d6e96fe6a2cf7cf95f9bbf13b79
Amit Buhnik <P0020755@ness-tech.co.il>
2018-03-21 15:39:14 +0200

    swdsds

--- app/events/event-details/event-route-activator.service.ts
+++ app/events/event-details/event-route-activator.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, CanDeactivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 
-import { EventService } from '../shared/event.service';
+// import { EventService } from '../shared/event.service';
 import { EventCreateComponent } from '../event-create.component';
 
 @Injectable()
 export class EventRouteActivator implements /* CanActivate, */ CanDeactivate<EventCreateComponent> {
 
-    constructor(private eventservice: EventService,
-        private router: Router) {
+    constructor(/* private eventservice: EventService,
+        private router: Router */) {
     }
 
     // canActivate(route: ActivatedRouteSnapshot): boolean {
