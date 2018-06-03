ecfadd7cff6ab2069ac839165d9153d9ecff0c8c
Amit Buhnik <P0020755@ness-tech.co.il>
2018-06-03 12:07:51 +0300

    dfdfdf

--- app/events/event-details/event-route-activator.service.ts
+++ app/events/event-details/event-route-activator.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, CanDeactivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 
-// import { EventService } from '../shared/event.service';
 import { EventCreateComponent } from '../event-create.component';
 
 @Injectable()
